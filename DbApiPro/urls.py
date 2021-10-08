4ef5fefa19aceab3a5f7a212a01848d3a90fa9ec
shakil102030 <shakilahmmed165@gmail.com>
2021-10-09 00:58:07 +0600

    FirstCommit

---
+++ DbApiPro/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('Account.urls')),
+    path('api/', include('ProjectInfo.urls')),
+    #path('api/', include('Api.urls')),
+    
+  
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
